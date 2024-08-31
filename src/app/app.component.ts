2ded36df7a349561006f2b4582a4f340a08f5444
Ovuoke Aghwotu <aghwotuovuoke@gmail.com>
2024-08-31 17:24:36 +0100

    build(spartan-ui): setup accordion & table ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AccordionComponent } from './accordion.component';
+import { DataTableComponent } from './table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [AccordionComponent, DataTableComponent],
+  template: `
+    <ng-accordion />
+
+    <ng-data-table />
+  `,
 })
 export class AppComponent {
   title = 'ng-spartan-ui-table';
